1bbb50a25e8b8cdced2b19f70e35fa734a19d614
KharchenkoSanya <kharchenko1996oleksandr@gmail.com>
2023-02-20 10:51:53 +0100

    Created OleksandrKharchenkoFactory

---
+++ UIMentoringSession/MenteesList/OleksandrKharchenkoFactory.swift
@@ -0,0 +1,17 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import UIKit
+
+final class OleksandrKharchenkoFactory: MenteeChallengesFactory {
+    var menteeName = "Oleksandr Kharchenko"
+    
+    func uiScreenFor(challenge: Challenge) -> UIViewController? {
+        switch challenge {
+        case .coffeeShopList:
+            return nil
+            
+        case .login:
+            return nil
+        }
+    }
+}
