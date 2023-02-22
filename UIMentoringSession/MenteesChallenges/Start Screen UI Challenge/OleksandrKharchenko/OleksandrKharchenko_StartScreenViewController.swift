ecc59ab81d87bdff83299570513ae8011b66cded
KharchenkoSanya <kharchenko1996oleksandr@gmail.com>
2023-02-22 20:53:09 +0100

    Finished startscreen

--- UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_StartScreenViewController.swift
+++ UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_StartScreenViewController.swift
@@ -1,30 +1,16 @@
-//
-//  OleksandrKharchenko_StartScreenViewController.swift
-//  UIMentoringSession
-//
-//  Created by Sashka IOS Developer on 22.02.2023.
 //  Copyright © 2023 Almost Engineer. All rights reserved.
-//
 
 import UIKit
 
 class OleksandrKharchenko_StartScreenViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var centralLabel: UILabel!
+    @IBOutlet weak var LetsGoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        LetsGoButton.layer.cornerRadius = 28
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
