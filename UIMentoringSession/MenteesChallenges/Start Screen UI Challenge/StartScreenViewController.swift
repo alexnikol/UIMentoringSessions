6da059c5a8b85ec2346444f07ef3921e2f349955
Yefim <efim.yakunin@gmail.com>
2023-02-22 10:50:45 +0200

    Add all elements in view

--- UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/StartScreenViewController.swift
+++ UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/StartScreenViewController.swift
@@ -2,15 +2,38 @@ import UIKit
 
 class StartScreenViewController: UIViewController {
     
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var middleLabel: UILabel!
+    
+    @IBOutlet weak var letsGoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadXib()
+        
+        // Set up the top label
+        topLabel.text = "takee"
+        topLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        topLabel.textColor = UIColor.white
+        
+        // Set up the middle label
+        middleLabel.text = "Happiness is closer than you think"
+        middleLabel.font = UIFont.systemFont(ofSize: 32)
+        middleLabel.textColor = UIColor.white
+        
+        // Set up the LetsGo button
+        letsGoButton.setTitle("Let's Go!", for: .normal)
+        letsGoButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        letsGoButton.setTitleColor(UIColor.white, for: .normal)
+        letsGoButton.layer.cornerRadius = 30
+        letsGoButton.layer.masksToBounds = true
     }
     
-    func loadXib() -> UIView? {
-      if let view = Bundle.main.loadNibNamed("StartScreenViewController", owner: self, options: nil)?.first as? UIView {
-            return view
-        }
-        return nil
+    init() {
+        super.init(nibName: "StartScreenViewController", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
