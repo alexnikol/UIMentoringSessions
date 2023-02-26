cb4cfdd8477f8a3738474dd335dd6e26d5621485
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-02-26 23:21:38 +0100

    Added 3 images and custom color to assets, button medium, changed the LogotipTakee, set all texts in controller, make final for class

--- UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_StartScreenViewController.swift
+++ UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_StartScreenViewController.swift
@@ -2,14 +2,14 @@
 
 import UIKit
 
-class OleksandrKharchenko_StartScreenViewController: UIViewController {
-
+final class OleksandrKharchenko_StartScreenViewController: UIViewController {
     @IBOutlet weak var centralLabel: UILabel!
     @IBOutlet weak var letsGoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        centralLabel.text = "Happiness is closer \nthan you think"
+        letsGoButton.setTitle("Let's Go!", for: .normal)
         letsGoButton.layer.cornerRadius = 28
     }
 }
