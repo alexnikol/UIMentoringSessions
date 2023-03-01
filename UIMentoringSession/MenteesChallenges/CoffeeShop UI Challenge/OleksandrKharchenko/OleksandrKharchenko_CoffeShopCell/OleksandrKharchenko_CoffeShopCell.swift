f5df38160e170d99d9934ce40e8b5c5b9904f45c
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-03-01 11:00:38 +0100

    Finished CoffeShops

---
+++ UIMentoringSession/MenteesChallenges/CoffeeShop UI Challenge/OleksandrKharchenko/OleksandrKharchenko_CoffeShopCell/OleksandrKharchenko_CoffeShopCell.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+
+import UIKit
+
+final class OleksandrKharchenko_CoffeShopCell: UITableViewCell {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var secondaryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
