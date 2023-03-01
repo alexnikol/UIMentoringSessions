ed704ec0f05c5f57dac33213b3c5ea925042e593
Yefim <efim.yakunin@gmail.com>
2023-03-01 12:05:34 +0200

    Add Main button class

---
+++ UIMentoringSession/MenteesChallenges/CoffeeShop UI Challenge/YefimYakunin/YefimYakuninCoffeShopChallengeViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class YefimYakuninCoffeShopChallengeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Coffee shops"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.sizeToFit()
+    }
+}
