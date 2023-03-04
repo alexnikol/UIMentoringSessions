be2728233573f9703eb69ad5eae64cfc9814b057
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-03-04 12:35:11 +0100

    Added Gradient to background view

--- UIMentoringSession/MenteesChallenges/Gaming Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_GamingScreenViewController.swift
+++ UIMentoringSession/MenteesChallenges/Gaming Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_GamingScreenViewController.swift
@@ -6,7 +6,20 @@ class OleksandrKharchenko_GamingScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setGradientLayer()
     }
+    
+    fileprivate func setGradientLayer() {
+        let gradient = CAGradientLayer()
+        let colorTop = UIColor(red: 0.33, green: 0.94, blue: 0.80, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 0.00, green: 0.40, blue: 1.00, alpha: 1.0).cgColor
+        gradient.colors = [colorTop, colorBottom]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.frame = view.bounds
+        view.layer.addSublayer(gradient)
+      }
     
     init() {
         super.init(nibName: "OleksandrKharchenko_GamingScreenViewController", bundle: nil)
