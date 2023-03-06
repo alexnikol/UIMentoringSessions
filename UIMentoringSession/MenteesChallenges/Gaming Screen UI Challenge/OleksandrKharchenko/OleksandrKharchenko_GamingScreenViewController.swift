4eaa9b5246322c0a8b2266d3c0d1d0ae566edbcd
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-03-06 09:46:00 +0100

    Added Stack View

--- UIMentoringSession/MenteesChallenges/Gaming Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_GamingScreenViewController.swift
+++ UIMentoringSession/MenteesChallenges/Gaming Screen UI Challenge/OleksandrKharchenko/OleksandrKharchenko_GamingScreenViewController.swift
@@ -4,22 +4,34 @@ import UIKit
 
 class OleksandrKharchenko_GamingScreenViewController: UIViewController {
 
+    
+    @IBOutlet private weak var firstView: UIView!
+    @IBOutlet private weak var gamingLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        descriptionLabel.text = "By creating an account or signing you agree to our Terms and Conditions."
         setGradientLayer()
+
+//        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+//        let underlineAttributedString = NSAttributedString(string: "Terms and Conditions.", attributes: underlineAttribute)
+//        descriptionLabel.attributedText = underlineAttributedString
     }
     
-    fileprivate func setGradientLayer() {
+    
+    
+    
+    private func setGradientLayer() {
         let gradient = CAGradientLayer()
         let colorTop = UIColor(red: 0.33, green: 0.94, blue: 0.80, alpha: 1.0).cgColor
         let colorBottom = UIColor(red: 0.00, green: 0.40, blue: 1.00, alpha: 1.0).cgColor
         gradient.colors = [colorTop, colorBottom]
         gradient.startPoint = CGPoint(x: 0.5, y: 0)
         gradient.endPoint = CGPoint(x: 1, y: 1)
-        gradient.frame = view.bounds
-        view.layer.addSublayer(gradient)
-      }
+        gradient.frame = firstView.bounds
+        firstView.layer.insertSublayer(gradient, at: 0)
+    }
     
     init() {
         super.init(nibName: "OleksandrKharchenko_GamingScreenViewController", bundle: nil)
---
+++ UIMentoringSession/MenteesChallenges/Gaming Screen UI Challenge/OleksandrKharchenko/View/OleksandrKharchenko_MainsButtons.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+
+import UIKit
+
+class OleksandrKharchenko_MainsButtons: UITableViewCell {
+
+    @IBOutlet weak var mainsButtons: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
