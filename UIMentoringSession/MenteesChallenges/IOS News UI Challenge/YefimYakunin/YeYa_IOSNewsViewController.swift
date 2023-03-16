7f76d1f12d84039046f8e3d4b5e218ed652f6677
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-03-16 10:58:23 +0100

    Added Group

---
+++ UIMentoringSession/MenteesChallenges/IOS News UI Challenge/YefimYakunin/YeYa_IOSNewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YeYa_IOSNewsViewController.swift
+//  UIMentoringSession
+//
+//  Created by Sashka IOS Developer on 16.03.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import UIKit
+
+class YeYa_IOSNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
