ce9a71e6274f21dac785d96181dd15a2cbd9dd75
Maksym Bilyk <black.bro1401@gmail.com>
2023-05-24 13:48:17 +0300

    Made screen

---
+++ UIMentoringSession/MenteesChallenges/Start Screen UI Challenge/Bilyk/MaksymBilyk_StartScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MaksymBilyk_StartScreenViewController.swift
+//  UIMentoringSession
+//
+//  Created by Maxos on 5/24/23.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import UIKit
+
+final class MaksymBilyk_StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    init() {
+        super.init(nibName: String(describing: MaksymBilyk_StartScreenViewController.self), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
