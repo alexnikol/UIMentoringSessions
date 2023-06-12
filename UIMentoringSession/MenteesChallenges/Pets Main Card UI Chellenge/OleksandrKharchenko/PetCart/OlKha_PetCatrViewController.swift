2051316b067a1c8b956ce2f452ac5a6ffed0c2e9
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-06-12 20:08:44 +0200

    Problem cornerRadius

--- UIMentoringSession/MenteesChallenges/Pets Main Card UI Chellenge/OleksandrKharchenko/PetCart/OlKha_PetCatrViewController.swift
+++ UIMentoringSession/MenteesChallenges/Pets Main Card UI Chellenge/OleksandrKharchenko/PetCart/OlKha_PetCatrViewController.swift
@@ -1,11 +1,31 @@
 import UIKit
 
 final class OlKha_PetCatrViewController: UIViewController {
-
-    @IBOutlet weak var testButton: UIButton!
+    @IBOutlet weak var oldYearPetCartLabel: UILabel!
+    @IBOutlet weak var weightPetCartLabel: UILabel!
+    @IBOutlet weak var CommandsPetCartLabel: UILabel!
     
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var dogOnTheStreetView: UIView!
+    @IBOutlet weak var willieDogImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        setupLayoutLabel()
+    }
+    
+    private func setupLayoutLabel() {
+        oldYearPetCartLabel.layer.cornerRadius = 20
+        willieDogImage.layer.cornerRadius = 32
+        dogOnTheStreetView.layer.cornerRadius = 16
+        dogOnTheStreetView.backgroundColor = RColor.pinkColor()
+        mainButton.layer.cornerRadius = 28
+    }
+    
+    init() {
+        super.init(nibName: RNib.olKha_PetCatrViewController.name, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
