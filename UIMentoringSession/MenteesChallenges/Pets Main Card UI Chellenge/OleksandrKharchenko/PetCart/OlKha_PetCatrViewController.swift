6d79fec46e7ed86294744007dd335304b209a23d
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-06-19 22:23:54 +0200

    Added scroll view

--- UIMentoringSession/MenteesChallenges/Pets Main Card UI Chellenge/OleksandrKharchenko/PetCart/OlKha_PetCatrViewController.swift
+++ UIMentoringSession/MenteesChallenges/Pets Main Card UI Chellenge/OleksandrKharchenko/PetCart/OlKha_PetCatrViewController.swift
@@ -1,24 +1,24 @@
 import UIKit
 
 final class OlKha_PetCatrViewController: UIViewController {
-    @IBOutlet weak var oldYearPetCartLabel: UILabel!
-    @IBOutlet weak var weightPetCartLabel: UILabel!
-    @IBOutlet weak var CommandsPetCartLabel: UILabel!
-    
-    @IBOutlet weak var mainButton: UIButton!
-    @IBOutlet weak var dogOnTheStreetView: UIView!
-    @IBOutlet weak var willieDogImage: UIImageView!
+//    @IBOutlet weak var oldYearPetCartLabel: UILabel!
+//    @IBOutlet weak var weightPetCartLabel: UILabel!
+//    @IBOutlet weak var CommandsPetCartLabel: UILabel!
+//    
+//    @IBOutlet weak var mainButton: UIButton!
+//    @IBOutlet weak var dogOnTheStreetView: UIView!
+//    @IBOutlet weak var willieDogImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayoutLabel()
     }
     
     private func setupLayoutLabel() {
-        oldYearPetCartLabel.layer.cornerRadius = 20
-        willieDogImage.layer.cornerRadius = 32
-        dogOnTheStreetView.layer.cornerRadius = 16
-        dogOnTheStreetView.backgroundColor = RColor.pinkColor()
-        mainButton.layer.cornerRadius = 28
+//        oldYearPetCartLabel.layer.cornerRadius = 20
+//        willieDogImage.layer.cornerRadius = 32
+//        dogOnTheStreetView.layer.cornerRadius = 16
+//        dogOnTheStreetView.backgroundColor = RColor.pinkColor()
+//        mainButton.layer.cornerRadius = 28
     }
     
     init() {
