5f7ba053f6e4de0a1b504ca8a79effe49bf10b2a
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-06-27 10:22:15 +0200

    Created folder OlKha_PetInformationCell

---
+++ UIMentoringSession/MenteesChallenges/Pets Main Card UI Chellenge/OleksandrKharchenko/PetCart/OlKha_PetInformationCell/OlKha_PetInformationCell.swift
@@ -0,0 +1,18 @@
+//
+//  OlKha_PetInformationCell.swift
+//  UIMentoringSession
+//
+//  Created by Sashka IOS Developer on 27.06.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import UIKit
+
+class OlKha_PetInformationCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
